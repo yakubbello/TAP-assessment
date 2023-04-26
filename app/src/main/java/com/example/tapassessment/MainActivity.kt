b03f467efbcc2d938f55346b73bedc990aba2f1d
Yakub Bello <mac@Yakubs-MacBook-Pro.local>
2023-04-26 16:27:45 +0100

    splashscreen fixed

--- app/src/main/java/com/example/tapassessment/MainActivity.kt
+++ app/src/main/java/com/example/tapassessment/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.tapassessment
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.tapassessment.databinding.ActivityMainBinding
+import com.example.tapassessment.viewmodel.MovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MovieViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val splashScreen = installSplashScreen()
         setContentView(binding.root)
+    }
+
+    override fun setSupportActionBar(toolbar: Toolbar?) {
+        super.setSupportActionBar(toolbar)
+//        toolbar?.title =
     }
 }
